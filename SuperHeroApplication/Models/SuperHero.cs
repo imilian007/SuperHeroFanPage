78c93e59df8cb724ea1895724ade9cba088e940a
imilian007 <maxketter01@gmail.com>
2018-10-24 10:48:08 -0500

    initial commit

---
+++ SuperHeroApplication/Models/SuperHero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroApplication.Models
+{
+    public class SuperHero
+    {
+        [Key]
+
+        public string Name { get; set; }
+
+        public string AlterEgo { get; set; }
+
+        public string PrimaryAbility { get; set; }
+
+        public string SecondaryAbilities{ get; set; }
+
+        public string CatchPhrase { get; set; }
+    }
+}
