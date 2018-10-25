aaa9e4bd3d82ac086db17df2971b36ebb216207c
imilian007 <maxketter01@gmail.com>
2018-10-25 14:04:53 -0500

    finished create and started index view

--- SuperHeroApplication/Controllers/SuperHeroController.cs
+++ SuperHeroApplication/Controllers/SuperHeroController.cs
@@ -1,39 +1,51 @@
-﻿using System;
+﻿using SuperHeroApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace SuperHeroApplication.Controllers
 {
     public class SuperHeroController : Controller
     {
+
+        ApplicationDbContext db = new ApplicationDbContext();
+        
         // GET: SuperHero
         public ActionResult Index()
         {
-            return View();
+            List<SuperHero> SuperHeroes = new List<SuperHero>();
+
+            SuperHeroes = db.SuperHeroes.ToList();
+            
+            return View(SuperHeroes);
         }
 
         // GET: SuperHero/Details/5
         public ActionResult Details(int id)
         {
-            ViewData.All();
             return View();
         }
 
         // GET: SuperHero/Create
-        public ActionResult Create()
+        public ActionResult Create(FormCollection collection)
         {
             return View();
         }
 
         // POST: SuperHero/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(SuperHero superHero)
         {
             try
             {
-                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    db.SuperHeroes.Add(superHero);
+                    db.SaveChanges();
+                }
 
                 return RedirectToAction("Index");
             }
@@ -44,18 +56,22 @@ namespace SuperHeroApplication.Controllers
         }
 
         // GET: SuperHero/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(string id)
         {
             return View();
         }
 
         // POST: SuperHero/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(string id, FormCollection collection)
         {
             try
             {
-                // TODO: Add update logic here
+                if (ModelState.IsValid)
+                {
+                    db.Entry(id).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
 
                 return RedirectToAction("Index");
             }
@@ -66,19 +82,17 @@ namespace SuperHeroApplication.Controllers
         }
 
         // GET: SuperHero/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(string id)
         {
             return View();
         }
 
         // POST: SuperHero/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(string id, FormCollection collection)
         {
             try
             {
-                // TODO: Add delete logic here
-
                 return RedirectToAction("Index");
             }
             catch
